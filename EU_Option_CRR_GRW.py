413e267b38b4f0502f407db53b8999da6e5bb967
Michel Vanderhulst <michelvanderhuls@hotmail.com>
2023-11-17 12:23:29 +0100

    Added rebalancing at maturity

--- EU_Option_CRR_GRW.py
+++ EU_Option_CRR_GRW.py
@@ -33,10 +33,6 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
 
     if  S < 0 or K < 0  or tree__periods < 1:
         return [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]
-    # nbrofsharesLabel, cashLabel, portfolioLabel, optionpriceLabel, intrinsicLabel, stocksLabel, 
-    #edge_x, edge_y, node_x, node_y, round(u,2), round(d,2), round(probUp,2), round(probDown,2), edge_y_Stock, 
-    # node_y_Stock, edge_y_Intrinsic, node_y_Intrinsic, edge_y_Optionprice, node_y_Optionprice, edge_y_Portfolio, node_y_Portfolio, 
-    # edge_y_Cash, node_y_Cash, edge_y_NbrOfShares, node_y_NbrOfShares, tree__periods :27 returns
     
 
     if CallOrPut == "Call":
@@ -51,12 +47,9 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
     ####################################################################################################################
     #####################                  START accounts initialization                           #####################
 
-    G_Stock, G_Intrinsic, G_Price, G_Portfolio, G_CashAccount, G_Shares = nx.Graph(), nx.Graph(), nx.Graph(), \
-                                                                          nx.Graph(), nx.Graph(), nx.Graph()
+    G_Stock, G_Intrinsic, G_Price, G_Portfolio, G_CashAccount, G_Shares = nx.Graph(), nx.Graph(), nx.Graph(), nx.Graph(), nx.Graph(), nx.Graph()
 
-    stockprices, optionintrinsicvalue, optionprice, labelStocks, labelsIntrinsic, labelsOptionPrice, CashAccount, \
-    NbrOfShares, Portfolio, labelPortfolio, labelCash, labelNbrOfShares, EquityAccount, \
-    labelEquity = {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+    stockprices, optionintrinsicvalue, optionprice,  CashAccount, NbrOfShares, Portfolio = {}, {}, {}, {}, {}, {}
 
     ## For the spatial chart
     edge_Y_stock, edge_Y_intrinsic, edge_Y_optionprice, edge_Y_portfolio, edge_Y_cash, edge_Y_nbrofshares = {}, {}, {}, {}, {}, {}
@@ -107,30 +100,15 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
                 stockprices[(i + 1, j)] = stockprices[(i, j)] * u
                 stockprices[(i + 1, j + 1)] = stockprices[(i, j)] * d
 
-                # labelStocks[(i, j)] = round(stockprices[(i, j)], 5)
-                # labelStocks[(i + 1, j)] = round(stockprices[(i + 1, j)], 5)
-                # labelStocks[(i + 1, j + 1)] = round(stockprices[(i + 1, j + 1)],5)
-
                 # option intrinsic value at all nodes
                 optionintrinsicvalue[(i, j)] = max(phi * (stockprices[(i, j)] - K), 0)
                 optionintrinsicvalue[(i + 1, j)] = max(phi * (stockprices[(i + 1, j)] - K), 0)
                 optionintrinsicvalue[(i + 1, j + 1)] = max(phi * (stockprices[(i + 1, j + 1)] - K), 0)
 
-                # labelsIntrinsic[(i, j)] = round(optionintrinsicvalue[(i, j)], 5)
-                # labelsIntrinsic[(i + 1, j)] = round(optionintrinsicvalue[(i + 1, j)], 5)
-                # labelsIntrinsic[(i + 1, j + 1)] = round(optionintrinsicvalue[(i + 1, j + 1)], 5)
-
-                # for the spatial chart
-                # edge_Y_stock[(i,j),(i+1,j)] = (labelStocks[(i, j)],labelStocks[(i + 1, j)])
-                # edge_Y_stock[(i, j), (i + 1, j + 1)] = (labelStocks[(i, j)],labelStocks[(i + 1, j + 1)])
-                # edge_Y_intrinsic[(i, j), (i + 1, j)] = (labelsIntrinsic[(i, j)], labelsIntrinsic[(i + 1, j)])
-                # edge_Y_intrinsic[(i, j), (i + 1, j + 1)] = (labelsIntrinsic[(i, j)], labelsIntrinsic[(i + 1, j + 1)])
-
                 edge_Y_stock[(i,j),(i+1,j)] = (stockprices[(i, j)],stockprices[(i + 1, j)])
                 edge_Y_stock[(i, j), (i + 1, j + 1)] = (stockprices[(i, j)],stockprices[(i + 1, j + 1)])
                 edge_Y_intrinsic[(i, j), (i + 1, j)] = (optionintrinsicvalue[(i, j)], optionintrinsicvalue[(i + 1, j)])
                 edge_Y_intrinsic[(i, j), (i + 1, j + 1)] = (optionintrinsicvalue[(i, j)], optionintrinsicvalue[(i + 1, j + 1)])
-
 
 
     # option price at all nodes
@@ -140,51 +118,25 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
             # values at maturity are not discounted given we start there
             if i == tree__periods:
                 optionprice[(i, j)] = optionintrinsicvalue[(i, j)]
-                # labelsOptionPrice[(i, j)] = round(optionprice[(i, j)], 5)
-
 
             # all the others, until price at t=0
             else:
                 optionprice[(i, j)] = discFact * (probUp * optionprice[(i + 1, j)] + probDown * optionprice[(i + 1, j + 1)])
-                # labelsOptionPrice[(i, j)] = round(optionprice[(i, j)], 5)
-
-                # for the spatial chart
-                # edge_Y_optionprice[(i, j), (i + 1, j)] = (labelsOptionPrice[(i, j)], labelsOptionPrice[(i + 1, j)])
-                # edge_Y_optionprice[(i, j), (i + 1, j + 1)] = (labelsOptionPrice[(i, j)], labelsOptionPrice[(i + 1, j + 1)])
 
                 edge_Y_optionprice[(i, j), (i + 1, j)] = (optionprice[(i, j)], optionprice[(i + 1, j)])
                 edge_Y_optionprice[(i, j), (i + 1, j + 1)] = (optionprice[(i, j)], optionprice[(i + 1, j + 1)])
 
 
-
-
-
     Portfolio[(0, 1)] = optionprice[(0, 1)]
-    # labelPortfolio[(0, 1)] = str(round(Portfolio[(0, 1)], 5))
 
     # changer l'algo qui tourne autour des nodes, car trop lent 
     for i in range(0, tree__periods + 1):
         for j in range(1, i + 2):
             if i < tree__periods:
-                # print(i, j)
+ 
                 # initialize / After Rebalancing
                 NbrOfShares[(i, j)] = (optionprice[(i + 1, j)] - optionprice[(i + 1, j + 1)]) / (stockprices[(i + 1, j)] - stockprices[(i + 1, j + 1)])
                 CashAccount[(i, j)] = Portfolio[(i, j)] - NbrOfShares[(i, j)] * stockprices[(i, j)]
-
-
-                # Labels for the graph
-                # if i == 0:
-                #     labelCash[(i, j)] = str(round(CashAccount[(i, j)], 5))
-                #     labelNbrOfShares[(i, j)] = str(round(NbrOfShares[(i, j)], 5))
-
-                # labelCash[(i, j)] += f",{round(CashAccount[(i, j)], 2)}"
-                # labelNbrOfShares[(i, j)] += f",{round(NbrOfShares[(i, j)], 2)}"
-                # labelPortfolio[(i, j)] += f",{round(CashAccount[(i, j)] + NbrOfShares[(i, j)] * stockprices[(i, j)], 2)}"
-
-                # labelCash[(i, j)] = f"{round(CashAccount[(i, j)], 5)}"
-                # labelNbrOfShares[(i, j)] = f"{round(NbrOfShares[(i, j)], 5)}"
-                # labelPortfolio[(i, j)] = f"{round(CashAccount[(i, j)] + NbrOfShares[(i, j)] * stockprices[(i, j)], 5)}"
-
 
                 # Before Rebalancing
                 NbrOfShares[(i + 1, j)] = NbrOfShares[(i, j)]
@@ -195,28 +147,15 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
                 CashAccount[(i + 1, j + 1)] = CashAccount[(i, j)] * np.exp(rf * step)
                 Portfolio[(i + 1, j + 1)] = CashAccount[(i + 1, j + 1)] + NbrOfShares[(i + 1, j + 1)] * stockprices[(i + 1, j + 1)]
 
-                # Labels for the graph
-                # labelCash[(i + 1, j)] = f"{round(CashAccount[(i + 1, j)], 5)}"
-                # labelCash[(i + 1, j + 1)] = f"{round(CashAccount[(i + 1, j + 1)], 5)}"
-
-                # labelNbrOfShares[(i + 1, j)] = f"{round(NbrOfShares[(i + 1, j)], 5)}"
-                # labelNbrOfShares[(i + 1, j + 1)] = f"{round(NbrOfShares[(i + 1, j + 1)], 5)}"
-
-                # labelPortfolio[(i + 1, j + 1)] = f"{round(Portfolio[(i + 1, j + 1)], 5)}"
-                # labelPortfolio[(i + 1, j)] = f"{round(Portfolio[(i + 1, j)], 5)}"
+            # performing rebalancing at tree period
+            elif i == tree__periods:
+                CashAccount[(i, j)] = CashAccount[(i, j)] + NbrOfShares[(i, j)] * stockprices[(i, j)]
+                NbrOfShares[(i, j)] = 0
 
     # re-running the loop now that we have the values for the spatial chart
     for i in range(0, tree__periods + 1):
         for j in range(1, i + 2):
             if i < tree__periods:
-                # edge_Y_portfolio[(i, j), (i + 1, j)] = (labelPortfolio[(i, j)], labelPortfolio[(i + 1, j)])
-                # edge_Y_portfolio[(i, j), (i + 1, j + 1)] = (labelPortfolio[(i, j)], labelPortfolio[(i + 1, j + 1)])
-
-                # edge_Y_cash[(i, j), (i + 1, j)] = (labelCash[(i, j)], labelCash[(i + 1, j)])
-                # edge_Y_cash[(i, j), (i + 1, j + 1)] = (labelCash[(i, j)], labelCash[(i + 1, j + 1)])
-
-                # edge_Y_nbrofshares[(i, j), (i + 1, j)] = (labelNbrOfShares[(i, j)], labelNbrOfShares[(i + 1, j)])
-                # edge_Y_nbrofshares[(i, j), (i + 1, j + 1)] = (labelNbrOfShares[(i, j)], labelNbrOfShares[(i + 1, j + 1)])
 
                 edge_Y_portfolio[(i, j), (i + 1, j)] = (Portfolio[(i, j)], Portfolio[(i + 1, j)])
                 edge_Y_portfolio[(i, j), (i + 1, j + 1)] = (Portfolio[(i, j)], Portfolio[(i + 1, j + 1)])
@@ -227,11 +166,6 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
                 edge_Y_nbrofshares[(i, j), (i + 1, j)] = (NbrOfShares[(i, j)], NbrOfShares[(i + 1, j)])
                 edge_Y_nbrofshares[(i, j), (i + 1, j + 1)] = (NbrOfShares[(i, j)], NbrOfShares[(i + 1, j + 1)])
 
-
-
-
-
-    # print(optionprice[(0,1)])
 
     # creating the nodes itself in the networkx graph
     pos = {}
@@ -265,12 +199,6 @@ def RepStrat_EU_Option_CRR_GRW(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
         x, y = pos[node]
         node_x.append(x)
         node_y.append(y)
-        # stocksLabel.append(labelStocks[node])
-        # intrinsicLabel.append(labelsIntrinsic[node])
-        # optionpriceLabel.append(labelsOptionPrice[node])
-        # portfolioLabel.append(labelPortfolio[node])
-        # cashLabel.append(labelCash[node])
-        # nbrofsharesLabel.append(labelNbrOfShares[node])
 
         stocksLabel.append(stockprices[node])
         intrinsicLabel.append(optionintrinsicvalue[node])
